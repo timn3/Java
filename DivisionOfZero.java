78bde4a00575ab8d814da18edeae806c2e2b84eb
timn3 <niertim308@gmail.com>
2017-04-05 12:08:38 +0200

    Update DivisionOfZero.java

--- DivisionOfZero.java
+++ DivisionOfZero.java
@@ -1,12 +1,12 @@
 public class DivisionOfZero {
 
 	public static void main(String[] args) {
-		double out; double num1 = 0.0; double num2 = 0.0; double num3 = 1.2;
-		out = num3 / num1; // Infinity
+		double out; double num1 = 0.0; double num2 = 1.2;
+		out = num2 / num1; // Infinity
 		System.out.println(out);
-		out = num1 / num2; // NaN
+		out = num1 / num1; // NaN
 		System.out.println(out);
-		out = num3 / num1 / num2; // Infinity
+		out = num2 / num1 / num1; // Infinity
 		System.out.println(out);
 	}
 
