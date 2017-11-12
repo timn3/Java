28ca7268cb1a64228be41cf31c1dc8122f54c9de
tim <tim@asus-chi>
2017-11-12 20:59:10 +0100

    Initial Commit

---
+++ readChar.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class readChar {
+  /*
+  readChar is a method which checks if a given String is one char long and if it contains the same value as a given Array.
+  The Array has to be included while calling the method.
+  */
+  public static char readChar(String[] chars) {
+    Scanner sc = new Scanner(System.in);
+    String read = null;
+    
+    outerloop:
+    while(true) {  
+      read = sc.next();
+        for (int i = 0; i < read.length(); i++) {
+          if (read.length() == 1 && read.contains(chars[i])){
+            break outerloop;
+          }   
+        }
+      System.out.println("Wrong input");
+      }
+    return read.charAt(0);
+  }
+  
+  public static void main(String[] args) {
+    readChar(/*Array*/);
+  }
+
+}
